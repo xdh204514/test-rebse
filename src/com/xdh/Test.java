c2cef96f72bba89f1c162dd8017167667ad0d4ad
xudehui <15707039958@qq.com>
2022-10-19 23:41:12 +0800

    feat：第一次提交

---
+++ src/com/xdh/Test.java
@@ -0,0 +1,16 @@
+package com.xdh;
+
+/**
+ * @author coderxdh
+ * @create 2022-10-19 23:37
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+    }
+
+    public void testFirstCommit() {
+        System.out.println("testFirstCommit");
+    }
+}
